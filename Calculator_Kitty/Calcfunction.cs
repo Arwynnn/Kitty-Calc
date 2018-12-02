26b3080c72886fb1b2369d9b3f26236e43938089
Joelp53 <joelparkinson@live.co.uk>
2018-12-02 22:33:11 +0000

    Initial commit

---
+++ Calculator_Kitty/Calcfunction.cs
@@ -0,0 +1,49 @@
+﻿namespace Calculator_Kitty
+{
+    public class Validate
+    {
+        public float Num { get; set; }
+        public bool Check { get; set; }
+    }
+    public static class Calcfunction
+    {
+        public static Validate IsValidNumber(string a)
+        {
+            var check = float.TryParse(a, out float b);
+            var validation = new Validate();
+            if (check)
+            {
+                validation.Num = b;
+                validation.Check = true;
+            }
+            return validation;
+        }
+
+        public static bool IsValidFunction(string a)
+        {
+            if (a == "+" || a == "-" || a == "*" || a == "/") return true;
+            return false;
+        }
+
+        public static float Add(float a, float b)
+        {
+            return a + b;
+        }
+
+        public static float Subtract(float a, float b)
+        {
+            return a - b;
+        }
+
+        public static float Multiply(float a, float b)
+        {
+            return a * b;
+        }
+
+        public static float Divide(float a, float b)
+        {
+            return a / b;
+        }
+
+    }
+}
---
+++ Calculator_Kitty/Program.cs
@@ -0,0 +1,91 @@
+﻿// Telling the program what part of the library you want to use
+using System;
+
+namespace Calculator_Kitty
+
+{   //An object that can stor methods such as static void main
+    class Program
+
+    {   //This is a method
+        static void Main(string[] args)
+        {   
+            //This is a loop (It'll loop forever until the program is closed)
+            while (true)
+            {
+
+                string function = String.Empty;
+                float num1 = 0; float num2 = 0;
+                bool check = false;
+
+                //say name of program
+                Console.WriteLine("Kitty Calc 2.0");
+
+                while (!check)
+                {
+                    Console.WriteLine("What is your first number?");
+                    var validate = Calcfunction.IsValidNumber(Console.ReadLine());
+                    if(validate.Check)
+                    {
+                        num1 = validate.Num;
+                        check = validate.Check;
+                    }
+                    else { Console.WriteLine("That was not a valid number.");}
+                }
+                check = false;
+
+                while (!check)
+                {
+                    Console.WriteLine("What is your second number?");
+                    var validate = Calcfunction.IsValidNumber(Console.ReadLine());
+                    if (validate.Check)
+                    {
+                        num2 = validate.Num;
+                        check = validate.Check;
+                    }
+                    else { Console.WriteLine("That was not a valid number."); }
+                }
+
+                check = false;
+
+                while (!check)
+                {
+                    Console.WriteLine("Would you like to add(+), subtract(-), multiply(*) or division(/)?");
+                    function = Console.ReadLine();
+                    if (Calcfunction.IsValidFunction(function))
+                    {
+                        check = Calcfunction.IsValidFunction(function);
+                    } else { Console.WriteLine("That is not a valid function.");
+                    }
+                       
+                }
+
+                switch (function)
+                {
+                    case "+":
+                        Console.WriteLine(Calcfunction.Add(num1, num2));
+                        break;
+                    case "-":
+                        Console.WriteLine(Calcfunction.Subtract(num1, num2));
+                        break;
+                    case "*":
+                        Console.WriteLine(Calcfunction.Multiply(num1, num2));
+                        break;
+                    case "/":
+                        Console.WriteLine(Calcfunction.Divide(num1, num2));
+                        break;
+                    default:
+                        Console.WriteLine("There seems to be an error.");
+                        break;
+                }
+                Console.WriteLine("Press any key to use again.\nOr type (quit/q) to quit the application.");
+                if (Console.ReadLine().ToLower() == "q" || Console.ReadLine().ToLower() == "quit")
+                {
+                    Environment.Exit(exitCode: 2);
+                }
+            }
+
+
+
+        }
+    }
+}
